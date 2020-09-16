fc666b152412e24fe510c33d98a1ce6792e5131d
Romeo <rmdavid@wah.ph>
2020-09-16 16:59:38 +0800

    add twicetagram

--- client/src/layouts/body/body.index.js
+++ client/src/layouts/body/body.index.js
@@ -10,12 +10,53 @@ class Body extends Component {
     data: null,
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then((res) => this.setState({ data: res.express }))
+      .catch((err) => console.log(err));
+  }
+  callBackendAPI = async () => {
+    const response = await fetch("/express_backend");
+    const body = await response.json();
 
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
   render() {
     return (
       <Container>
         <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="twice.png" />
+              <Card.Body>
+                <Card.Title>Signal 4th Mini Album</Card.Title>
+                <Card.Text>
+                  Signal is the fourth extended play (EP) by South Korean girl
+                  group Twice. The EP was released on May 15, 2017 by JYP
+                  Entertainment and distributed by Genie Music.
+                </Card.Text>
+                <Button variant="danger">Watch on YouTube</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="https://i.pinimg.com/originals/29/ce/20/29ce20baa4b0444db17040d78a55d4fe.jpg" />
+              <Card.Body>
+                <Card.Title>twicetagram 2nd Album</Card.Title>
+                <Card.Text>
+                  Signal is the fourth extended play (EP) by South Korean girl
+                  group Twice. The EP was released on May 15, 2017 by JYP
+                  Entertainment and distributed by Genie Music.
+                </Card.Text>
+                <Button variant="danger">Watch on YouTube</Button>
+              </Card.Body>
+            </Card>
+          </Col>
           <Col>
             <Card style={{ width: "18rem" }}>
               <Card.Img variant="top" src="twice.png" />
